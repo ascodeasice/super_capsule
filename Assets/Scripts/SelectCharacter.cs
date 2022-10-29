cfc83c78cd16762b6407615d53d2c80c888f5d12
ascodeasice <ab0988956087@gmail.com>
2022-10-29 15:31:49 +0800

    Change skin of character Turn into a capsule with two colors

---
+++ Assets/Scripts/SelectCharacter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectCharacter : MonoBehaviour
+{
+    public GameObject[] characters;
+    public int characterIndex = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
